2d75f27b7c8244cd2c8d518f246b1baa84b89254
edianibarrola <edianibarrola@gmail.com>
2021-04-28 00:43:30 +0000

    update

--- src/app.py
+++ src/app.py
@@ -27,15 +27,62 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
+    status = 200
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        members
-    }
+    try:
+        members = jackson_family.get_all_members()
+        response_body = members
+    except:
+        response_body = {
+            "message": "problem with the server."
+        }
+        status = 500
+
+    return jsonify(response_body), status
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    status = 200
+    body = request.json
+    if body is None:
+        response_body = {
+            "message": "body was missing from post."
+        }
+        status = 400
+    else:
+        try:
+            member= jackson_family.add_member(body)
+            response_body=member
+        except:
+            response_body = {
+                "message": "problem with the server."
+            }
+            status = 500
+    return jsonify(response_body), status
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    status = 200
+    
+    try:
+        member= jackson_family.delete_member(id)
+        if member == False:
+            response_body={
+                "message":"member not found"
+            }
+            status = 404
+        else:
+            response_body=True
+            
+            status = 500
+    except:
+        response_body={
+                "message":"problem with the server"
+            }
+        status = 500
+    return jsonify(response_body), status
 
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
